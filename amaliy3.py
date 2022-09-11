4315edba65e3ad5be067753fbac45bbc206292e2
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2022-09-11 19:39:16 +0500

    Changed cause uzbek words into english words

--- amaliy3.py
+++ amaliy3.py
@@ -3,45 +3,45 @@
 #Pythonda quyidagi o'zgaruvchilarni yaratin:
 
 #JAVOB
-kocha = "Bog'bon"
-mahalla = "Sag'bon"
-tuman = "Bodomzor"
-viloyat = "Samarqand"
+street = "Bog'bon"
+streettest = "Sag'bon"
+shtaat = "Bodomzor"
+city = "Samarqand"
 
-print(kocha, mahalla, tuman, viloyat)
+print(street, streettest, shtaat, city)
 
 #SAVOL
 #Yuqoridagi o'zgaruvchilarning qiymatini foydalanuvchidan so'rang. Va avvalgi mashqni takrorlang. 
 
 #JAVOB
 
-kocha = input("Ko'changizni nomini kiriting")
-mahalla = input("Mahallangizni nomini kiriting")
-tuman = input("Tumaningizni nomini kiriting")
-viloyat = input("Viloyatingizni nomini kiriting")
-print(kocha, mahalla, tuman, viloyat)
+street = input("Ko'changizni nomini kiriting")
+streettest = input("Mahallangizni nomini kiriting")
+shtaat = input("Tumaningizni nomini kiriting")
+city = input("Viloyatingizni nomini kiriting")
+print(street, streettest, shtaat, city)
 
 
 #SAVOL
 #Yuqoridagi matnni konsolga chiqarishda har bir verguldan keyin yangi satrdan yozing.
 
 #JAVOB
-kocha = "Bog'bon\n"
-mahalla = "Sag'bon\n"
-tuman = "Bodomzor\n"
-viloyat = "Samarqand"
+street = "Bog'bon\n"
+streettest = "Sag'bon\n"
+shtaat = "Bodomzor\n"
+city = "Samarqand"
 
-print(kocha, mahalla, tuman, viloyat)
+print(street, streettest, shtaat, city)
 
 #SAVOL
 #Yuqoridagi matnni f-string yordamida yangi, manzil deb nomlangan o'zgaruvchiga yuklang. 
 
-manzil = f"{kocha}, {mahalla}, {tuman}, {viloyat}"
+manzil = f"{street}, {streettest}, {shtaat}, {city}"
 print(manzil)
 
 #SAVOL 
 #manzil o'zgaruvchisiga yuqorida biz o'rgangan title(), upper(), lower(), capitalize() metodlarini qo'llab ko'ring.
 
 #JAVOB
-manzil = f"{kocha.title()}, {mahalla.upper()}, {tuman.lower()}, {viloyat.capitalize()}"
+manzil = f"{street.title()}, {streettest.upper()}, {shtaat.lower()}, {city.capitalize()}"
 print(manzil)
